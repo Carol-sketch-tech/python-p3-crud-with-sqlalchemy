e8bbcc94f0645e399faef981bb252ebd91fc059d
Caroline <bancroftwanjiru10000@gmail.com>
2024-09-19 12:29:54 +0300

    creating records, updating filtering, func, order_by, deleting and __repl__() in SQL alchemy

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -12,10 +12,66 @@ Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
-
+    __table_args__ = (
+        PrimaryKeyConstraint(
+            'id',
+            name = 'id_pk'
+        ),
+        UniqueConstraint(
+            'email',
+            name= 'unique_email'
+        ),
+        CheckConstraint(
+            'grade BETWEEN 1 AND 12',
+            name = 'grade_between_1_and_12'
+        )
+    )
+    Index('index_name', 'name' )
     id = Column(Integer(), primary_key=True)
     name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default = datetime.now())
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
+
+    # we use the engine to configure a sessions class
+    Session = sessionmaker(bind=engine)
+    # and we use the session class to configure a session engine.
+    session = Session()
+
+    albert_einstein = Student (
+        name = 'Albert Einstein',
+        email = 'albert.einstain@zurich.edu',
+        grade = 6,
+        birthday = datetime(
+            year = 1953,
+            month= 3,
+            day = 14
+        ),
+    )
+
+    caroline_wanjiru = Student(
+        name = 'caroline wanjiru',
+        email = 'caroline.wanjiru@moringaschool',
+        grade = 9,
+        birthday = datetime(
+            year = 2002,
+            month = 7,
+            day = 10
+        ),
+    )
+    session.bulk_save_objects([albert_einstein, caroline_wanjiru])
+    session.commit()
+
+    students = session.query(Student) # this query method is used to retrieve data against your databse using more pythonic interface.
+    grade = [grade for grade in session.query(caroline_wanjiru.grade)]
+
+    student_by_grade_desc = [student for student in session.query(Student.name, Student.grade).order_by(desc(Student.grade))]
+    print(f'New student ID is {albert_einstein.id}.')
+    print(f'The new stundets name is {caroline_wanjiru.name}')
+    print(grade)
+    print(student_by_grade_desc)
